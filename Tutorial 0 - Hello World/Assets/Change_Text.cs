cb8f8405f26a8c26b927714e6d7cb600ab99b8d4
Steven Fisher <Stevushka@gmail.com>
2020-01-19 04:47:38 -0500

    1st commit!

---
+++ Tutorial 0 - Hello World/Assets/Change_Text.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Change_Text : MonoBehaviour
+{
+    public Text newText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            newText.text = "Hello, my name is Steven Fisher!";
+        }
+
+        if(Input.GetKeyUp("space"))
+        {
+            newText.text = "Hello World!";
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
